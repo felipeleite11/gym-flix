004dd071aa87b078dcf01f7665a9dd0ee5ad47ae
Felipe Leite <lipi4shared@hotmail.com>
2026-06-11 02:56:47 -0300

    Bloqueando zoom.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/Providers";
@@ -18,6 +18,13 @@ export const metadata: Metadata = {
   title: "GYM Flix",
   description: "Plataforma para gestão e acompanhamento de treinos de musculação.",
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false
+}
 
 export default function RootLayout({
   children,
